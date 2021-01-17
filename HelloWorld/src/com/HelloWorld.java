c3451a74d160d1a4fce290342146f424c5ff68f5
KonYan <724343694@qq.com>
2021-01-17 15:34:48 +0800

    HelloWorld

---
+++ HelloWorld/src/com/HelloWorld.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author KonYan
+ * @create 2021-01-17 15:34
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
